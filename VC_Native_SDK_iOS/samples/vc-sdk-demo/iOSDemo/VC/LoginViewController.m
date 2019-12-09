791edc052a813b8ecd1f34d41aa8ab8d2a74b747
speakerl <speaker_l@163.com>
2019-12-09 16:10:45 +0800

    1.详细参会者退出，和本端退出的说明。 2.登陆之后不需要再配置账号。

--- VC_Native_SDK_iOS/samples/vc-sdk-demo/iOSDemo/VC/LoginViewController.m
+++ VC_Native_SDK_iOS/samples/vc-sdk-demo/iOSDemo/VC/LoginViewController.m
@@ -46,6 +46,38 @@
 
 - (void)VCRtc:(VCRtcModule *)module didIncomingCallInfo:(NSDictionary *)incomingInfo {
     NSLog(@"接收到被叫信息 ： %@", incomingInfo) ;
+    [self connectIncommingCall:incomingInfo] ;
+}
+
+- (void)connectIncommingCall:(NSDictionary *)incomingInfo {
+    //初始化
+    VCRtcModule *vcrtc = [VCRtcModule sharedInstance];
+    //配置服务器域名
+    vcrtc.apiServer = self.serverTF.text ;
+    //遵循 VCRtcModuleDelegate方法
+    vcrtc.delegate = self;
+    vcrtc.groupId = kGroupId;
+    //入会类型配置 点对点
+    [vcrtc configConnectType:VCConnectTypeMeeting];
+    //入会音视频质量配置
+    [vcrtc configVideoProfile:VCVideoProfile360P];
+    //入会接收流的方式配置
+    [vcrtc configMultistream:true];
+    [vcrtc configPrivateCloudPlatform:YES];
+    [vcrtc configPTPOneTimeToken:incomingInfo[@"token"] andBsskey:incomingInfo[@"bsskey"] andStamp:incomingInfo[@"time"] owner:@""] ;
+    //用户账号配置(用户登录需配置,未登录不需要)
+//    [vcrtc configLoginAccount:@"maxx@51vmr.cn"];
+    //配置音视频 channel: 用户地址 password: 参会密码 name: 会中显示名称 xiaobeioldone@zijingcloud.com
+    [vcrtc connectChannel:incomingInfo[@"conference_alias"] password:@"" name:@"马晓霞" success:^(id _Nonnull response) {
+        //记录此时会议状态
+        NSUserDefaults *userDefault = [[NSUserDefaults alloc]initWithSuiteName:kGroupId];
+        [userDefault setObject:@"inmeeting" forKey:kScreenRecordMeetingState];
+        
+    } failure:^(NSError * _Nonnull error) {
+        NSUserDefaults *userDefault = [[NSUserDefaults alloc]initWithSuiteName:kGroupId];
+        [userDefault setObject:@"outmeeting" forKey:kScreenRecordMeetingState];
+    }];
+    vcrtc.forceOrientation = UIDeviceOrientationLandscapeLeft;
 }
 
 /*
